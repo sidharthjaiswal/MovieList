316552d704b2748c992250ff2c5ac2832e17356b
Sidharth <sidhankit@gmail.com>
2023-12-17 20:30:34 +0100

    initialized Spring Boot

---
+++ backend/src/test/java/dev/sidharth/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sidharth.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
