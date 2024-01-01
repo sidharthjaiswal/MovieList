bce090df5945f8604dcc7190a4a66e9b9f0be154
Sidharth <sidhankit@gmail.com>
2024-01-02 00:16:50 +0100

    initialized react

--- backend/src/main/java/dev/sidharth/movies/MoviesApplication.java
+++ backend/src/main/java/dev/sidharth/movies/MoviesApplication.java
@@ -3,7 +3,6 @@ package dev.sidharth.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class MoviesApplication {
 	public static void main(String[] args) {
