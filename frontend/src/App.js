2033bf8ff94054e9d2196c9d1992cafeece79750
Sidharth <sidhankit@gmail.com>
2024-01-03 13:07:16 +0100

    bug fix in Reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/home/Home.js';
-import Header from './components/header/Header.js';
-import Trailer from './components/trailer/Trailer.js';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/notFound';
+
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
-      const response = await api.get("/movies");
+      const response = await api.get("/api/movies");
       console.log(response.data);
       setMovies(response.data);
     }
     catch (err) {
       console.log(err);
     }
+  }
+
+  const getMovieData = async (movieId) => {
+
+    try {
+      const response = await api.get(`/api/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+      
+    }
+    catch (error) {
+      console.error(error);
+    }
+
   }
 
   useEffect(() => {
@@ -34,6 +55,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />} ></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+          <Route path="*" element = {<NotFound/>}></Route>
         </Route>
       </Routes>
     </div>
--- frontend/src/components/reviews/Reviews.js
+++ frontend/src/components/reviews/Reviews.js
@@ -3,18 +3,16 @@ import api from '../../api/axiosConfig';
 import { useParams } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import ReviewForm from './ReviewForm.js';
+import React from 'react';
 
-import React from 'react'
-
-const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
-
+const Reviews = ({ getMovieData, movie, reviews = [], setReviews }) => {
     const revText = useRef();
     let params = useParams();
     const movieId = params.movieId;
 
     useEffect(() => {
         getMovieData(movieId);
-    }, [])
+    }, []);
 
     const addReview = async (e) => {
         e.preventDefault();
@@ -29,62 +27,56 @@ const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
             rev.value = "";
 
             setReviews(updatedReviews);
-        }
-        catch (err) {
+        } catch (err) {
             console.error(err);
         }
+    };
 
-        return (
-            <Container>
-                <Row>
-                    <Col><h3>Reviews</h3></Col>
-                </Row>
-                <Row className="mt-2">
-                    <Col>
-                        <img src={movie?.poster} alt="" />
-                    </Col>
-                    <Col>
-                        {
-                            <>
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h3>Reviews</h3>
+                </Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={movie?.poster} alt="" />
+                </Col>
+                <Col>
+                    <>
+                        <Row>
+                            <Col>
+                                <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review?" />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <hr />
+                            </Col>
+                        </Row>
+                        {reviews?.map((r, index) => (
+                            <React.Fragment key={index}>
                                 <Row>
-                                    <Col>
-                                        <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review?" />
-                                    </Col>
+                                    <Col>{r.body}</Col>
                                 </Row>
                                 <Row>
                                     <Col>
                                         <hr />
                                     </Col>
                                 </Row>
-                            </>
-                        }
-                        {
-                            reviews?.map((r) => {
-                                return (
-                                    <>
-                                        <Row>
-                                            <Col>{r.body}</Col>
-                                        </Row>
-                                        <Row>
-                                            <Col>
-                                                <hr />
-                                            </Col>
-                                        </Row>
-                                    </>
-                                )
-                            })
-                        }
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <hr />
-                    </Col>
-                </Row>
-            </Container>
-        )
+                            </React.Fragment>
+                        ))}
+                    </>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <hr />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
-
-    }
-}
-export default Reviews
+export default Reviews;
