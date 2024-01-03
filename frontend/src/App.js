2ccddd1de9ba382686a843e2882150bb6889ea7a
Sidharth <sidhankit@gmail.com>
2024-01-03 10:43:59 +0100

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home.js';
+import Header from './components/header/Header.js';
 
 function App() {
 
@@ -27,10 +28,10 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />} ></Route>
-
         </Route>
       </Routes>
     </div>
